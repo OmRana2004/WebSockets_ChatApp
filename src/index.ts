1cbc474be5f3b1befc73aa827521d1b63278ef56
OmRana2004 <omrana8755800322@gmail.com>
2025-06-16 00:13:30 +0530

    cleaning up the commit message file

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });  
 
-let userCount = 0;
 let allSockets: WebSocket[] = []; // [socket1, socket2, socket3, ...]
 
 wss.on("connection", (socket) => {
     allSockets.push(socket);
 
-    userCount = userCount + 1;
-    console.log("user connected #" + userCount);
+    console.log("user connected #");
 
     socket.on("message", (message) => {
         console.log("message received: " + message.toString());
