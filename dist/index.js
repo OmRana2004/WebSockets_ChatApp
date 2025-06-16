a1abc9c38f5d80b2dc5ba92abeb8c711318cc46a
OmRana2004 <omrana8755800322@gmail.com>
2025-06-16 17:41:55 +0530

    ./index.ts

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
 let allSockets = []; // [socket1, socket2, socket3, ...]
 wss.on("connection", (socket) => {
     allSockets.push(socket);
-    userCount = userCount + 1;
-    console.log("user connected #" + userCount);
+    console.log("user connected #");
     socket.on("message", (message) => {
         console.log("message received: " + message.toString());
         allSockets.forEach(s => {
@@ -15,3 +13,7 @@ wss.on("connection", (socket) => {
         });
     });
 });
+/*
+
+
+*/ 
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,25 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });  
 
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
 let allSockets: WebSocket[] = []; // [socket1, socket2, socket3, ...]
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
 
-    console.log("user connected #");
-
-    socket.on("message", (message) => {
-        console.log("message received: " + message.toString());
-        allSockets.forEach(s => {
-            s.send(message.toString() + ": sent from the server");
-        })
-    })
-})
+  socket.on("message", (message) => {
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
+}
+  })
+ socket.on("disconnect", () => {
+    allSockets = allSockets.filter(x => x != socket);
+  })
+  // Is To recover of params to cover of its installtion of its instute cover of its params of cover of recall to reduce and make its 
