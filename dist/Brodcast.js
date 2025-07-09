cb99bed7e3690023ca91955699b06770d9261a8f
OmRana2004 <omrana8755800322@gmail.com>
2025-07-09 12:33:47 +0530

    ./git/COMMIT_EDITMSG

---
+++ dist/Brodcast.js
@@ -0,0 +1,14 @@
+"use strict";
+// import { WebSocketServer, WebSocket } from "ws";
+// const wss = new WebSocketServer({ port: 8080 });  
+// let allSockets: WebSocket[] = []; // [socket1, socket2, socket3, ...]
+// wss.on("connection", (socket) => {
+//     allSockets.push(socket);
+//     console.log("user connected #");
+//     socket.on("message", (message) => {
+//         console.log("message received: " + message.toString());
+//         allSockets.forEach(s => {
+//             s.send(message.toString() + ": sent from the server");
+//         })
+//     })
+// })
